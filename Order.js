97a1fc6117ac97e428d3bb1dd5b04b9d6f6cfeab
MCapredoni <144966520+MCapredoni@users.noreply.github.com>
2025-02-27 21:38:53 +0000

    Adjustments and Exploration

--- Order.js
+++ Order.js
@@ -4,15 +4,15 @@ export class Order {
         WELCOMING: () => {
           let aReturn = [];
           this.stateCur = this.OrderState.RESERVING;
-          aReturn.push("Hello! Thanks for using food fast!");
-          aReturn.push("Tonights menu is TEMPFOOD! We have 2 options this evening TEMP1 and TEMP2. Please respond with the option you prefer.");
+          aReturn.push("Hello! Thanks for ordering Marcs Italian!");
+          aReturn.push("Tonights menu is Pizza! We have 2 options this evening Thin Crust Pizza and Regular Style Pizza. Please respond with the option you prefer.");
           return aReturn;
         },
         RESERVING: (sInput) => {
           let aReturn = [];
           this.isDone = true;
-          if (sInput.toLowerCase().startsWith('y')) {
-            aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          if (sInput.toLowerCase().startsWith('t')) {
+            aReturn.push(`Thin Crust it is! TEMPTEMPTEMPTEMPTEMP ${this.sFrom}`);
             let d = new Date();
             d.setMinutes(d.getMinutes() + 120);
             aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
