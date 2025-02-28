8a36c1ebf18192ead17e5a310dedc9696bfa7128
MCapredoni <144966520+MCapredoni@users.noreply.github.com>
2025-02-28 05:09:25 +0000

    Spec Updates

--- spec/order_spec.js
+++ spec/order_spec.js
@@ -6,18 +6,19 @@ describe("Tests all stages of an order", function() {
         const aResults = oOrder.handleInput("hello");
         expect(aResults[0]).toBe("Hello! Thanks for ordering Marcs Italian!")
     });
-    it("test thin crust", function() {
+    it("test pizza", function() {
         const oOrder = new Order("999-999-9999");
-        oOrder.handleInput("thin")
-        const aResults = oOrder.handleInput("thin");
-        expect(aResults[0]).toBe(`Thin Crust it is! Would you like your pizza a small or large?`)
+        oOrder.handleInput("pizza")
+        const aResults = oOrder.handleInput("pizza");
+        expect(aResults[0]).toBe(`Pizza it is! Would you like your pizza a small or large? Please reply with either "large" or "small"`)
     });
-    it("test small", function() {
+    it("test pasta", function() {
         const oOrder = new Order("999-999-9999");
-        oOrder.handleInput("small")
-        const aResults = oOrder.handleInput("small");
-        expect(aResults[0]).toBe(`Small pizza it is`)
+        oOrder.handleInput("pasta")
+        const aResults = oOrder.handleInput("pasta");
+        expect(aResults[0]).toBe(`Pasta it is! Would you like a small serving or large serving? Please reply with either "large" or "small"`)
     });
+    
   });
   
   
